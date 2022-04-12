93cc708d883f452a3094ac40066fae48870fbe0c
Thomas Ruppeiner <truppeiner@gmail.com>
2022-04-12 18:40:20 -0500

    updated port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals.json');
 
@@ -54,6 +55,6 @@ app.get('/api/animals',(req, res) =>{
 });
 
 
-app.listen(3001, () =>{
+app.listen(PORT, () =>{
     console.log('API server now on port 3001!');
 });
