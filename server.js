43af0581631e6af6856e0e05cd0445b303e83d7e
Thomas Ruppeiner <truppeiner@gmail.com>
2022-04-14 16:55:49 -0500

    lesson 3 complete

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require('./data/animals.json');
 const { json } = require('express/lib/response');
 
@@ -113,6 +114,21 @@ app.post('/api/animals', (req, res) =>{
   
 });
 
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) =>{
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () =>{
     console.log('API server now on port 3001!');
